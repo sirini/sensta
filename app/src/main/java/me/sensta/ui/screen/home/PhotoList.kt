f7314f83d96adb93d4704252287f2f84f037637a
sirini <sirini@gmail.com>
2025-03-21 11:10:51 +0900

    사진 목록 하단에 접근 시 이전 사진 목록들 가져오도록 개선, 게시글 가져오고 나서 약간의 딜레이와 Crossfade 애니메이션 추가 반영

--- app/src/main/java/me/sensta/ui/screen/home/PhotoList.kt
+++ app/src/main/java/me/sensta/ui/screen/home/PhotoList.kt
@@ -1,10 +1,10 @@
 package me.sensta.ui.screen.home
 
 import android.widget.Toast
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,6 +16,7 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -26,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.distinctUntilChanged
 import me.domain.model.gallery.TsboardPhoto
 import me.domain.repository.TsboardResponse
 import me.sensta.ui.screen.home.card.PostCard
@@ -51,46 +53,56 @@ fun PhotoList(
 
     // 스크롤 상태를 감시해서 마지막 항목에 도달하면 이전 사진들 불러오기
     LaunchedEffect(listState) {
-        snapshotFlow { listState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }.collect { index ->
-            index?.let {
-                if (index >= photos.size - 1) {
-                    viewModel.refresh(sinceUid = photos.last().uid)
-                }
-            }
-        }
-    }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pullRefresh(pullRefreshState)
-    ) {
-        // 사진 목록 보여주기
-        LazyColumn(
-            state = listState,
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(photos) { photo ->
-                PostCard(photo = photo)
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-
-            // 맨 아래로 이동 시 추가 데이터를 로드하는 도중에 로딩 화면 제공
-            item {
-                if (isLoading) {
-                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+        snapshotFlow { listState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
+            .distinctUntilChanged()
+            .collect { index ->
+                index?.let {
+                    if (index >= photos.size - 1) {
+                        val sinceUid = photos.dropLast(1).lastOrNull()?.uid ?: photos.last().uid
+                        viewModel.refresh(sinceUid = sinceUid)
+                        Toast.makeText(context, "이전 사진들을 불러왔습니다.", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
-        }
+    }
 
-        // 당겨서 새로고침 중일 때 로딩 화면 제공
-        PullRefreshIndicator(
-            refreshing = isLoading,
-            state = pullRefreshState,
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
+    Crossfade(targetState = !isLoading) { visible ->
+        if (visible) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .pullRefresh(pullRefreshState)
+            ) {
+
+                // 사진 목록 보여주기
+                LazyColumn(
+                    state = listState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(photos) { photo ->
+                        PostCard(photo = photo)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+
+                // 당겨서 새로고침 중일 때 로딩 화면 제공
+                PullRefreshIndicator(
+                    refreshing = isLoading,
+                    state = pullRefreshState,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.padding(16.dp),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
     }
 }
--- app/src/main/java/me/sensta/viewmodel/PhotoListViewModel.kt
+++ app/src/main/java/me/sensta/viewmodel/PhotoListViewModel.kt
@@ -1,8 +1,10 @@
 package me.sensta.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -28,15 +30,25 @@ class PhotoListViewModel @Inject constructor(
 
     // 갤러리 사진 목록 가져오기
     private fun loadPhotos(sinceUid: Int = 0) {
+        if (_isLoadingMore.value) return
+
         viewModelScope.launch {
+            _photos.value = TsboardResponse.Loading
+            _isLoadingMore.value = true
             getPhotoListUseCase(sinceUid = sinceUid).collect {
                 _photos.value = it
             }
+
+            delay(200)
+
+            _isLoadingMore.value = false
         }
     }
 
     // 갤러리 목록 업데이트
     fun refresh(sinceUid: Int = 0) {
         loadPhotos(sinceUid = sinceUid)
+
+        Log.d("DEBUG", "loadPhotos($sinceUid)") /// DEBUG
     }
 }
