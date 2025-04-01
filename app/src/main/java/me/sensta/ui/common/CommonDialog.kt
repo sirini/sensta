8ef542b61ac8a175234a085fb51aff617ef3b00b
sirini <sirini@gmail.com>
2025-04-01 19:14:54 +0900

    자주 사용하는 다이얼로그를 CommonDialog로 추상화

---
+++ app/src/main/java/me/sensta/ui/common/CommonDialog.kt
@@ -0,0 +1,82 @@
+package me.sensta.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit,
+    icon: ImageVector,
+    content: @Composable () -> Unit
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+            shape = MaterialTheme.shapes.large,
+            tonalElevation = 12.dp,
+            color = MaterialTheme.colorScheme.secondaryContainer
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = "Icon",
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .size(32.dp)
+                    )
+                }
+
+                content()
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextButton(
+                        onClick = onDismissRequest,
+                        modifier = Modifier.padding(end = 8.dp)
+                    ) {
+                        Text(text = "취소")
+                    }
+                    Button(onClick = onConfirm) {
+                        Text(text = "확인")
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/me/sensta/ui/screen/profile/ProfileViewEditNameDialog.kt
+++ app/src/main/java/me/sensta/ui/screen/profile/ProfileViewEditNameDialog.kt
@@ -1,31 +1,16 @@
 package me.sensta.ui.screen.profile
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import me.sensta.ui.common.CommonDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,55 +20,15 @@ fun ProfileViewEditNameDialog(
 ) {
     var newName by remember { mutableStateOf("") }
 
-    BasicAlertDialog(
+    CommonDialog(
         onDismissRequest = onDismissRequest,
+        onConfirm = { onConfirm(newName) },
+        icon = Icons.Default.Edit
     ) {
-        Surface(
-            modifier = Modifier
-                .wrapContentWidth()
-                .wrapContentHeight(),
-            shape = MaterialTheme.shapes.large,
-            tonalElevation = 12.dp,
-            color = MaterialTheme.colorScheme.secondaryContainer
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-            )
-            {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = "Edit name",
-                        modifier = Modifier
-                            .padding(bottom = 16.dp)
-                            .size(24.dp)
-                    )
-                }
-
-                OutlinedTextField(
-                    value = newName,
-                    onValueChange = { newName = it },
-                    label = { Text(text = "변경할 이름을 입력해 주세요") },
-                )
-
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    TextButton(onClick = onDismissRequest) {
-                        Text(text = "취소")
-                    }
-                    TextButton(onClick = { onConfirm(newName) }) {
-                        Text(text = "확인")
-                    }
-                }
-            }
-        }
+        OutlinedTextField(
+            value = newName,
+            onValueChange = { newName = it },
+            label = { Text(text = "변경할 이름을 입력해 주세요") },
+        )
     }
 }
