1878d6a5f76da5faa9f0e61ed68b7da44bb0ca02
sirini <sirini@gmail.com>
2025-05-05 21:08:42 +0900

    앱 아이콘 변경

--- app/src/main/java/me/sensta/ui/screen/ExplorerScreen.kt
+++ app/src/main/java/me/sensta/ui/screen/ExplorerScreen.kt
@@ -1,5 +1,6 @@
 package me.sensta.ui.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +17,6 @@ import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,10 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import me.domain.repository.TsboardResponse
 import me.sensta.ui.common.LocalScrollBehavior
-import me.sensta.ui.navigation.common.LocalSnackbar
 import me.sensta.ui.screen.explorer.GridImage
 import me.sensta.ui.screen.explorer.SearchBox
 import me.sensta.viewmodel.local.LocalExplorerViewModel
@@ -38,9 +38,9 @@ import me.sensta.viewmodel.uievent.ExplorerUiEvent
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun ExplorerScreen() {
+    val context = LocalContext.current
     val scrollBehavior = LocalScrollBehavior.current
     val explorerViewModel = LocalExplorerViewModel.current
-    val snackbar = LocalSnackbar.current
     val isLoading by remember { mutableStateOf(false) }
     val posts by explorerViewModel.posts
     val pullRefreshState = rememberPullRefreshState(
@@ -57,7 +57,7 @@ fun ExplorerScreen() {
         explorerViewModel.uiEvent.collect { event ->
             when (event) {
                 is ExplorerUiEvent.UnableToFindPosts -> {
-                    snackbar.showSnackbar("게시글을 찾을 수 없습니다", "확인", duration = SnackbarDuration.Short)
+                    Toast.makeText(context, "게시글을 더 찾을 수 없습니다", Toast.LENGTH_SHORT).show()
                     notFound = true
                 }
             }
