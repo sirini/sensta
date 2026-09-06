51b57b93d8c2c324ba1b0b4271355ee66fd2592f
sirini <sirini@gmail.com>
2026-09-06 14:38:19 +0900

    Stabilize Android chat IME layout

--- app/src/main/java/me/sensta/ui/screen/user/ChatInputBar.kt
+++ app/src/main/java/me/sensta/ui/screen/user/ChatInputBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -38,7 +39,9 @@ fun ChatInputBar() {
     Surface(
         tonalElevation = 2.dp,
         color = MaterialTheme.colorScheme.surfaceContainer,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .imePadding()
     ) {
         Column(
             modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
